3da8bcd1099df449b76ffe91c8d501b12984f734
zsh142857 <zhengshunhe142857@gmail.com>
2022-03-11 14:49:03 +0800

    20220311

--- src/main/java/com/tom/Scoring.java
+++ src/main/java/com/tom/Scoring.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student sam = new Student();
-        sam.name="Sam";
-        sam.english=75;
-        sam.math=91;
+        Student sam = new Student("Sam",75,91);
+     //  sam.name="Sam";
+        //  sam.english=75;
+       // sam.math=91;
+        sam.print();
     }
 }
--- src/main/java/com/tom/Student.java
+++ src/main/java/com/tom/Student.java
@@ -4,5 +4,15 @@ public class Student {
     String name;
     int english;
     int math;
-    
+    public Student(String name,int english,int math){
+        this.name=name;
+        this.english=english;
+        this.math=math;
+    }
+    public Student(){
+
+    }
+    public void print() {
+        System.out.println(name + "\t" + english + "\t" + math);
+    }
 }
