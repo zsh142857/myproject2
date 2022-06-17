3d12675b219d63ffd5bab9422a754d2f71b50bbf
zsh142857 <zhengshunhe142857@gmail.com>
2022-06-17 16:35:59 +0800

    ScoringFile 02

--- src/main/java/com/tom/score/ScoringFile.java
+++ src/main/java/com/tom/score/ScoringFile.java
@@ -1,6 +1,7 @@
 package com.tom.score;
 
 import javax.imageio.IIOException;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,12 +10,18 @@ public class ScoringFile {
     public static void main(String[] args) {
       try {
           FileReader fileReader= new FileReader("score.txt");
-          int data =fileReader.read();
-          while (data !=-1){
-              System.out.println((char) data);
-              data =fileReader.read();
+          BufferedReader reader= new BufferedReader(fileReader);
+          String line= reader.readLine();
+          while (line!= null){
+              //System.out.println(line);
+              String[] token = line.split(",");
+              String name = token[0];
+              int english =Integer.parseInt(token[1]);
+              int math = Integer.parseInt(token[2]);
+              Student stu =new Student(name,english,math);
+              stu.print();
+              line=reader.readLine();
           }
-
       }catch (FileNotFoundException e){
           System.out.println("資料讀取失敗");
       }catch (IOException e){
